634afc2c0a45cdcf634c87d3a08c3c929f1d6fee
nguyenthithuthao24538 <128467883+nguyenthithuthao24538@users.noreply.github.com>
2023-04-11 17:17:02 +0700

    Add files via upload

---
+++ lap1..sql
@@ -0,0 +1,55 @@
+--câu 1--
+CREATE FUNCTION SanPham(@tenHang nvarchar(50))
+RETURNS TABLE
+AS
+RETURN
+SELECT sp.masp, sp.tensp, sp.soluong, sp.mausac, sp.giaban, sp.donvitinh, sp.mota
+FROM SANPHAM sp
+INNER JOIN HANGSX hs ON sp.mahangsx = hs.mahangsx
+WHERE hs.tenhang = @tenHang
+
+SELECT * FROM DBO.SanPham(N'SAMSUNG')
+
+
+--câu 3--
+CREATE FUNCTION LuaChon(@luachon int)
+RETURNS @bang TABLE (tensp nvarchar(20), masp nvarchar(10),tenhang nvarchar(20), Soluong int)
+AS
+BEGIN
+	IF @luachon = 0
+	BEGIN
+		INSERT INTO @bang 
+			SELECT tensp, Sanpham.masp, tenhang, Soluong FROM Sanpham INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+			WHERE soluong < 0
+	END
+	IF @luachon = 1
+	BEGIN
+		INSERT INTO @bang
+			SELECT tensp, Sanpham.masp, tenhang, Soluong FROM Sanpham INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.mahangsx
+			WHERE soluong > 0
+	END
+	RETURN
+END
+--câu 4--
+CREATE FUNCTION ThongTinNV (@Phong nvarchar(30))
+RETURNS TABLE RETURN
+SELECT Tennv FROM Nhanvien
+WHERE Nhanvien.Phong = @Phong
+GO
+SELECT *FROM ThongTinNV ('Vat tu')
+--câu 6--
+CREATE FUNCTION DSXuat (@x int, @y int)
+RETURNS TABLE RETURN
+SELECT Tenhang, tensp, soluongX
+FROM Xuat INNER JOIN Sanpham ON Xuat.Masp = Sanpham.masp INNER JOIN Hangsx ON Sanpham.mahangsx = Hangsx.Mahangsx
+WHERE YEAR(Ngayxuat) BETWEEN @x AND @y
+GO
+SELECT * FROM DSXuat(2019, 2020)
+--câu 8--
+CREATE FUNCTION NVNhap (@x int)
+RETURNS TABLE RETURN
+SELECT Nhanvien.Manv, Tennv, Phong
+FROM Nhanvien INNER JOIN Nhap ON Nhanvien.Manv = Nhap.Manv
+WHERE DAY(Ngaynhap) = @x
+GO
+SELECT * FROM NVNhap (22)
